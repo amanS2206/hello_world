b43429299545afe6f8b178d639d897989b0ce86b
amanS2206 <aman.saxena_cs18@gla.ac.in>
2020-08-24 03:10:46 +0530

    add home page to the application

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Aman app',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -20,14 +21,20 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: ,
+      home: Myhomepage() ,
     );
   }
 }
 
+class Myhomepage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold();
+  }
+}
